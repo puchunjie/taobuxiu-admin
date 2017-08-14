9af3f7f57655ca9a05ee51f7966e4e66c7a05037
puchunjie <puchunjie@live.com>
2017-08-14 10:06:47 +0800

    增加订单删选功能

--- src/components/buyer/order/index.vue
+++ src/components/buyer/order/index.vue
@@ -8,13 +8,13 @@
         </Tab-pane>
         <Tab-pane :label="'等卖家确认('+ waitForConfirm +')'">
             <div class="table-content">
-                <order-listtable :dataList="waitForConfirmData"></order-listtable>
+                <order-listtable :dataList="orderData"></order-listtable>
                 <Page :total="waitForConfirm" show-total v-show="waitForConfirm>0" @on-change="pageChange" :current="currentPage+1"></Page>
             </div>
         </Tab-pane>
         <Tab-pane :label="'待评价('+ waitForVote +')'">
             <div class="table-content">
-                <order-listtable :dataList="waitForVoteData"></order-listtable>
+                <order-listtable :dataList="orderData"></order-listtable>
                 <Page :total="waitForVote" show-total v-show="waitForVote>0" @on-change="pageChange" :current="currentPage+1"></Page>
             </div>
         </Tab-pane>
@@ -34,12 +34,11 @@ export default {
         return{
             tableHeight:0,
             orderData:[],
-            waitForConfirmData:[],
-            waitForVoteData:[],
             waitForConfirm:0,
             waitForVote:0,
             maxCount:0,
-            currentPage:0
+            currentPage:0,
+            status: -1
         }
     },
     created () {
@@ -58,21 +57,16 @@ export default {
             this.$http.get(myOrders,{
                 params:{
                     currentPage: this.currentPage,
-                    pageCount:10
+                    pageCount:10,
+                    status: this.status
                 }
             }).then(res => {
                 if(res.data.status === 0){
                     let data = res.data.data;
                     this.orderData = data.orders;
-                    this.waitForVoteData = data.orders.filter(el => {
-                        return el.status === 1
-                    });
-                    this.waitForConfirmData = data.orders.filter(el => {
-                        return el.status === 0
-                    });
                     this.waitForVote = data.waitForVote;
                     this.waitForConfirm = data.waitForConfirm;
-                    this.maxCount = data.maxCount;
+                    this.maxCount = data.allCounts;
                 }else{
                     this.$Message.error(res.data.errorMsg)
                 }
@@ -84,7 +78,21 @@ export default {
             this.currentPage = page - 1;
             this.getOrderData();
         },
-        reset(){
+        reset(name){
+            console.log(name)
+            switch (name) {
+                case 0:
+                    this.status = -1;
+                    break;
+                case 1:
+                    this.status = 0;
+                    break;
+                case 2:
+                    this.status = 1;
+                    break;
+                default:
+                    break;
+            }
             this.currentPage = 0;
             this.getOrderData();
         }
--- src/components/seller/order/index.vue
+++ src/components/seller/order/index.vue
@@ -8,20 +8,20 @@
         </Tab-pane>
         <Tab-pane :label="'待确认('+ waitForConfirm +')'">
             <div class="table-content">
-                <order-listtable :dataList="waitForConfirmData"></order-listtable>
+                <order-listtable :dataList="orderData"></order-listtable>
                 <Page :total="waitForConfirm" show-total v-show="waitForConfirm>0" @on-change="pageChange" :current="currentPage+1"></Page>
             </div>
         </Tab-pane>
         <Tab-pane :label="'待评价('+ waitForEvaluate +')'">
             <div class="table-content">
-                <order-listtable :dataList="waitForEvaluateData"></order-listtable>
+                <order-listtable :dataList="orderData"></order-listtable>
                 <Page :total="waitForEvaluate" show-total v-show="waitForEvaluate>0" @on-change="pageChange" :current="currentPage+1"></Page>
             </div>
         </Tab-pane>
         <Tab-pane :label="'已评价('+ evaluate +')'">
             <div class="table-content">
-                <order-listtable :dataList="evaluateData"></order-listtable>
-                <Page :total="evaluate" show-total v-show="evaluate>0"></Page>
+                <order-listtable :dataList="orderData"></order-listtable>
+                <Page :total="evaluate" show-total v-show="evaluate>0" @on-change="pageChange"></Page>
             </div>
         </Tab-pane>
     </Tabs>
@@ -40,14 +40,12 @@ export default {
         return{
             tableHeight:0,
             orderData:[],
-            waitForConfirmData:[],//待确认
-            waitForEvaluateData:[],//待评价
-            evaluateData:[],//已评价
             waitForConfirm:0,
             waitForEvaluate:0,
             evaluate:0,
             maxCount:0,
-            currentPage: 0
+            currentPage: 0,
+            status: -1
         }
     },
     created () {
@@ -66,34 +64,40 @@ export default {
             this.$http.get(commingOrders,{
                 params:{
                     currentPage: this.currentPage,
-                    pageCount:10
+                    pageCount:10,
+                    status: this.status
                 }
             }).then(res => {
-                let Data = res.data.data;
-                this.orderData = Data.orders;
-                // 待确认列表
-                this.waitForConfirmData = Data.orders.filter(el => {
-                    return el.status === 0
-                });
-                // 待评价列表
-                this.waitForEvaluateData = Data.orders.filter(el => {
-                    return el.status === 1
-                });
-                // 已评价列表
-                this.evaluateData = Data.orders.filter(el => {
-                    return el.status === 2
-                });
-                this.waitForConfirm = this.waitForConfirmData.length;
-                this.waitForEvaluate = this.waitForEvaluateData.length;
-                this.evaluate = this.evaluateData.length;
-                this.maxCount = Data.maxCount;
+                let data = res.data.data;
+                this.orderData = data.orders;
+                this.waitForConfirm = data.waitForConfirm;
+                this.waitForEvaluate = data.waitEvaluate;
+                this.evaluate = data.hasEvaluate;
+                this.maxCount = data.allCounts;
             });
         },
         pageChange(page){
             this.currentPage = page - 1;
             this.getCartData();
         },
-        reset(){
+        reset(name){
+            switch (name) {
+                case 0:
+                    this.status = -1;
+                    break;
+                case 1:
+                    this.status = 0;
+                    break;
+                case 2:
+                    this.status = 1;
+                    break;
+                case 3:
+                    this.status = 2;
+                    break;
+                default:
+                    this.status = -1;
+                    break;
+            }
             this.currentPage = 0;
             this.getCartData();
         }
